e4eb054a10a1ad6d294756b94d000964bc119646
Mustafa Gamal <mustafagamal35@gmail.com>
2025-05-07 21:37:33 +0300

    Summary

---
+++ lang/ar/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'هذه البيانات لا تطابق سجلاتنا.',
+    'throttle' => 'عدد المحاولات لتسجيل الدخول كثير جدًا. يرجى المحاولة مرة أخرى في :seconds ثانية.',
+    'login' => 'تسجيل الدخول',
+    'register' => 'تسجيل حساب',
+    'email' => 'البريد الإلكتروني',
+    'password' => 'كلمة المرور',
+    'remember_me' => 'تذكرني',
+    'forgot_password' => 'نسيت كلمة المرور؟',
+    'confirm_password' => 'تأكيد كلمة المرور',
+    'name' => 'الاسم',
+    'logout' => 'تسجيل الخروج',
+
+];
